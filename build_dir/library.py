c6424d55082d147dcadce986649a421ec5cc2ced
Kevin Johnson <krjohnson111@gmail.com>
2020-08-07 22:42:14 -0400

    Make library a data class

---
+++ build_dir/library.py
@@ -0,0 +1,55 @@
+import dataclasses
+import pathlib
+from typing import List, Optional
+
+from . import util
+
+
+TYPE_STRING = {'type': 'string'}
+TYPE_STRING_ARRAY = {'type': 'array', 'items': TYPE_STRING}
+
+LIBRARY_SCHEMA = {
+    'type': 'object',
+    'additionalProperties': {
+        'type': 'object',
+        'properties': {
+            'find_package': TYPE_STRING_ARRAY,
+            'pkg_check': TYPE_STRING_ARRAY,
+            'include_dirs': TYPE_STRING_ARRAY,
+            'link_dirs': TYPE_STRING_ARRAY,
+            'link_libs': TYPE_STRING_ARRAY
+        },
+        'additionalProperties': False,
+        'required': ['link_libs']
+    }
+}
+
+
+@dataclasses.dataclass
+class Library:
+    name: str
+    find_package: List[str]
+    pkg_check: List[str]
+    include_dirs: List[pathlib.Path]
+    link_dirs: List[pathlib.Path]
+    link_libs: List[str]
+
+
+def load(path: pathlib.Path) -> List[Library]:
+    def paths_array(value: Optional[List[str]]) -> List[pathlib.Path]:
+        if value is None:
+            return []
+        return [pathlib.Path(path) for path in value]
+
+    data = util.load_json(path, LIBRARY_SCHEMA)
+    return [
+        Library(
+            name=name,
+            find_package=fields.get('find_package', []),
+            pkg_check=fields.get('pkg_check', []),
+            include_dirs=paths_array(fields.get('include_dirs')),
+            link_dirs=paths_array(fields.get('link_dirs')),
+            link_libs=fields.get('link_libs', [])
+        )
+        for name, fields in data.items()
+    ]
