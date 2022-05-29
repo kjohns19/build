31c1d30e9711812f4f3d4bc04562bd49bea052ef
Kevin Johnson <krjohnson111@gmail.com>
2022-05-29 17:12:12 -0400

    Output build files to current directory

--- src/build/__main__.py
+++ src/build/__main__.py
@@ -6,25 +6,32 @@ import sys
 
 def main() -> int:
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    if not pathlib.Path('./CMakeLists.txt').exists():
+
+    current_dir = pathlib.Path.cwd()
+
+    if not (current_dir / 'CMakeLists.txt').exists():
         logging.error('No CMakeLists.txt file found in current directory!')
         return 1
 
-    build_dir = pathlib.Path('./build.dir')
+    build_dir = current_dir / 'build.dir'
     build_dir.mkdir(exist_ok=True)
-    rc = run_cmake(build_dir)
+    rc = run_cmake(build_dir=build_dir, output_dir=current_dir)
     if rc:
         return rc
     rc = run_make(build_dir, sys.argv[1:])
     return rc
 
 
-def run_cmake(build_dir: pathlib.Path) -> int:
-    proc = subprocess.run(['cmake', '..'], cwd=build_dir)
+def run_cmake(build_dir: pathlib.Path, output_dir: pathlib.Path) -> int:
+    logging.info('Running cmake')
+    proc = subprocess.run(
+        ['cmake', f'-DCMAKE_RUNTIME_OUTPUT_DIRECTORY={output_dir}', '..'],
+        cwd=build_dir)
     proc.returncode
 
 
 def run_make(build_dir: pathlib.Path, argv: list[str]) -> int:
+    logging.info('Running make')
     proc = subprocess.run(['make'] + argv, cwd=build_dir)
     proc.returncode
 
