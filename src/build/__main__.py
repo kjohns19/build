45dfb51adbaa5727e493e11762cbf4bbe960cfe1
Kevin Johnson <krjohnson111@gmail.com>
2023-10-07 10:17:02 -0400

    Add action support, add ctest action

--- src/build/__main__.py
+++ src/build/__main__.py
@@ -1,14 +1,21 @@
 import argparse
 import dataclasses
+import enum
 import logging
 import pathlib
 import subprocess
 import sys
 
 
+class Action(enum.Enum):
+    MAKE = 'make'
+    CTEST = 'ctest'
+
+
 @dataclasses.dataclass
 class Args:
     sudo: bool
+    action: Action
 
 
 def main() -> int:
@@ -23,6 +30,29 @@ def main() -> int:
         return 1
 
     build_dir = current_dir / 'build.dir'
+
+    actions = {
+        Action.MAKE: action_make,
+        Action.CTEST: action_ctest,
+    }
+    return actions[args.action](current_dir, build_dir, args, argv)
+
+
+def parse_args() -> tuple[Args, list[str]]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--sudo', action='store_true',
+        help='run make using sudo')
+    parser.add_argument(
+        'action', nargs='?', choices=[a.value for a in Action],
+        default=Action.MAKE.value,
+        help='action to run')
+    args, remaining = parser.parse_known_args()
+    return Args(args.sudo, Action(args.action)), remaining
+
+
+def action_make(current_dir: pathlib.Path, build_dir: pathlib.Path, args: Args,
+                argv: list[str]) -> int:
     build_dir.mkdir(exist_ok=True)
     rc = run_cmake(build_dir=build_dir, output_dir=current_dir)
     if rc:
@@ -32,13 +62,11 @@ def main() -> int:
     return rc
 
 
-def parse_args() -> tuple[Args, list[str]]:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--sudo', action='store_true',
-        help='run make using sudo')
-    args, remaining = parser.parse_known_args()
-    return Args(args.sudo), remaining
+def action_ctest(current_dir: pathlib.Path, build_dir: pathlib.Path, args: Args,
+                 argv: list[str]) -> int:
+    logging.info('Running ctest')
+    proc = subprocess.run(['ctest'] + argv, cwd=build_dir)
+    return proc.returncode
 
 
 def run_cmake(build_dir: pathlib.Path, output_dir: pathlib.Path) -> int:
@@ -46,7 +74,7 @@ def run_cmake(build_dir: pathlib.Path, output_dir: pathlib.Path) -> int:
     proc = subprocess.run(
         ['cmake', f'-DCMAKE_RUNTIME_OUTPUT_DIRECTORY={output_dir}', '..'],
         cwd=build_dir)
-    proc.returncode
+    return proc.returncode
 
 
 def run_make(build_dir: pathlib.Path, sudo: bool, argv: list[str]) -> int:
@@ -55,7 +83,7 @@ def run_make(build_dir: pathlib.Path, sudo: bool, argv: list[str]) -> int:
     if sudo:
         cmd = ['sudo'] + cmd
     proc = subprocess.run(cmd, cwd=build_dir)
-    proc.returncode
+    return proc.returncode
 
 
 if __name__ == '__main__':
